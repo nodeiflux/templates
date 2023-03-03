a7f40e01c8109a6ec7d2f486b85fbe4effbb3ea1
Nicholai Nissen <nicholainissen@gmail.com>
2023-03-03 19:12:45 +0100

    style(curveball): order of imports

--- curveball/src/app.ts
+++ curveball/src/app.ts
@@ -1,13 +1,14 @@
 import { Application } from '@curveball/core'
-import type { Middleware } from '@curveball/core'
 import problem from '@curveball/problem'
 import bodyParser from '@curveball/bodyparser'
 import { pinoLogger } from 'pino-curveball'
 
+import type { Middleware } from '@curveball/core'
+
 export const createApp = (routes: Middleware[]): Application => {
   const app = new Application()
-  app.use(problem()) /* Handle errors with the application/problem+json standard. */
   app.use(pinoLogger())
+  app.use(problem()) /* Handle errors with the application/problem+json standard. */
   app.use(bodyParser())
   app.use(...routes)
 
