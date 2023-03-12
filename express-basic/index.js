2819ab62f7734e56c430b198b4c36b28b2f43d02
Nicholai Nissen <nicholainissen@gmail.com>
2023-03-12 19:29:14 +0100

    feat(express-basic): add dotenv features

--- express-basic/index.js
+++ express-basic/index.js
@@ -1,16 +1,27 @@
+#!/usr/bin/env node
+'use strict'
+
+require('dotenv').config()
+
 const app = require('./app')
 const routes = require('./routes')
 
 const port = process.env.PORT ?? 8000
 
-async function main () {
+function main () {
   console.log('Application starting')
 
-  const server = app.load(routes).listen(port, () => {
-    console.info(`Application listening on port ${port}`)
-  })
-
-  server.on('error', console.error.bind(console))
+  app.load(routes)
+    .listen(port, () => {
+      console.info(`Application listening on port ${port}`)
+    })
+    .on('error', error => {
+      console.error(error)
+    })
 }
 
-main()
+if (process.env.NODE_ENV !== 'test') main()
+
+module.exports = {
+  main
+}
