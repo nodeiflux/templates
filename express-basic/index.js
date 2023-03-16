bc4a66f75bbd8e53c1e562155f74d5f168500e7b
Nicholai Nissen <nicholainissen@gmail.com>
2023-03-16 10:54:48 +0100

    feat(express-basic): add database initialising

--- express-basic/index.js
+++ express-basic/index.js
@@ -5,12 +5,13 @@ require('dotenv').config()
 
 const app = require('./app')
 const routes = require('./routes')
+const db = require('./database')
 
 const port = process.env.PORT ?? 8000
 
-function main () {
+async function main () {
   console.log('Application starting')
-
+  const database = await initialiseDatabase(process.env.NODE_ENV, process.env.DB_URI)
   app.load(routes)
     .listen(port, () => {
       console.info(`Application listening on port ${port}`)
@@ -18,6 +19,18 @@ function main () {
     .on('error', error => {
       console.error(error)
     })
+}
+
+/**
+ * Initialise database for this application.
+ * @param {'production'|'test'|'development'} env Node environment
+ * @param {string?} dbUri Database uri
+ * @returns
+ */
+function initialiseDatabase (env, dbUri) {
+  if (env === 'production') return db.createSQLDatabase(dbUri)
+  if (env === 'test') return db.createMemoryDatabase()
+  return db.createSQLiteDatabase()
 }
 
 if (process.env.NODE_ENV !== 'test') main()
